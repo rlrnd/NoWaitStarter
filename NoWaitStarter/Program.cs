4618aa3b5b2af43e72f86e58c67c05a1edcd0d3d
Alexiel Mejias <alexielm@rogers.com>
2019-06-25 07:57:41 -0400

    Initial Code Commit

---
+++ NoWaitStarter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoWaitStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var compiler = new Process())
+            {
+                compiler.StartInfo.FileName = args.First();
+                compiler.StartInfo.Arguments = String.Join(" ", args.Skip(1));
+                compiler.StartInfo.UseShellExecute = true;
+                compiler.Start();
+            }
+        }
+    }
+}
